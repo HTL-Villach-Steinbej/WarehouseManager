2259c155d88c547277d3d881d0c5c73749979dc9
EliasMikula <Deepolis123>
2019-03-22 19:09:54 +0100

    Added Remember me to Login

---
+++ app/src/main/java/com/example/warehousemanager/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.warehousemanager;
+
+
+import com.google.firebase.firestore.auth.User;
+
+public class Prevalent {
+    public static User currentOnlineUser;
+
+    public static final String UserEmailKey="test@gmail.com";
+    public static final String UserPasswordKey="Passwort123";
+
+}
