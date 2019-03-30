8d9579d64958771540a32c58845062b4df8c4674
HTL-Villach-Steinbej <jonathan.steinberger@edu.htl-villach.at>
2019-03-30 17:50:00 +0100

    Second try to implement the ViewWarehouse Activity

--- app/src/main/java/Activities/WatchWarehouse.java
+++ app/src/main/java/Activities/WatchWarehouse.java
@@ -1,10 +1,15 @@
 package Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.warehousemanager.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class WatchWarehouse extends AppCompatActivity {
 
@@ -12,5 +17,21 @@ public class WatchWarehouse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_warehouse);
+
+        final GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+
+        Button addPasswordButton = (Button)findViewById(R.id.btnAddRegal);
+        addPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                int childCount = gridLayout.getChildCount();
+
+                AppCompatButton btnRegal = new AppCompatButton(context);
+                btnRegal.setText("Regal" + (childCount + 1));
+                gridLayout.addView(btnRegal, childCount);
+
+            }
+        });
     }
 }
